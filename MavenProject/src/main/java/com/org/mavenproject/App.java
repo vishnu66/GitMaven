e41a4c3a1af5b5d95c5eb97db87c13977ce62c31
vishnu.unnikrishnan <vishnu.unnikrishnan@fingent.com>
2017-07-24 12:27:17 +0530

    Page Object Model Added

--- MavenProject/src/main/java/com/org/mavenproject/App.java
+++ MavenProject/src/main/java/com/org/mavenproject/App.java
@@ -1,13 +1,32 @@
 package com.org.mavenproject;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    private static WebElement element = null;
+    
+    public static WebElement textbox_Username(WebDriver driver)
     {
-        System.out.println( "Hello World!" );
+    	element = driver.findElement(By.id("email"));
+    	return element;
+    }
+    
+    public static WebElement textbox_Password(WebDriver driver)
+    {
+    	element = driver.findElement(By.id("password"));
+    	return element;
+    }
+    
+    public static WebElement LoginButton(WebDriver driver)
+    {
+    	element = driver.findElement(By.cssSelector(".btn.btn-primary.btn-raised.login-button"));
+    	return element;
     }
 }
--- MavenProject/src/test/java/Test/NewTest.java
+++ MavenProject/src/test/java/Test/NewTest.java
@@ -1,17 +1,23 @@
 package Test;
 
 import org.testng.annotations.Test;
+
+import com.org.mavenproject.App;
+
 import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 
 public class NewTest {
-	WebDriver gk;
+  private static WebDriver gk = null;
   @Test
-  public void f() 
+  public void Login() 
   {
 	  gk.get("http://mfsapp-testing.fingent.net/");
+	  App.textbox_Username(gk).sendKeys("mfssuperadm@gmail.com");
+	  App.textbox_Password(gk).sendKeys("12345678");
+	  App.LoginButton(gk).click();
   }
   @BeforeTest
   public void beforeTest() 
@@ -24,7 +30,7 @@ public class NewTest {
   @AfterTest
   public void afterTest() 
   {
-	  gk.close();
+	  gk.quit();
   }
 
 }
