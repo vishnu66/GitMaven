44467efb319c028c7be4b3459acdc137c7f01b4a
vishnu.unnikrishnan <vishnu.unnikrishnan@fingent.com>
2017-08-01 11:26:57 +0530

    Excel file added to Project folder

--- MavenProject/src/main/java/utility/Constant1.java
+++ MavenProject/src/main/java/utility/Constant1.java
@@ -5,7 +5,8 @@ public class Constant1
 	public static final String URL ="http://mfsapp-testing.fingent.net/";
 	public static final String Username = "mfssuperadm@gmail.com";
 	public static final String Password = "12345678";
-	
-	public static final String Excel_Path = "D://Tools//Selenium,Jenkins,Maven Integration//";
-	public static final String Excel_File = "LoginExcel.xlsx";
+	//Please copy the excel file to your Project folder
+	public static final String Excel_Path = System.getProperty("user.dir");
+	//Please provide the name of Excel file below
+	public static final String Excel_File = "\\LoginExcel.xlsx";
 }
